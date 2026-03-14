326b73afa9a50b617594359e9d8a2437a8083cbc
sherdorbek <abduqahhorovsherdorbek770@gmail.com>
2026-03-14 12:20:19 -0700

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,19 @@ import { SiTailwindcss } from "react-icons/si";
 import { FaJsSquare } from "react-icons/fa";
 import { SiNextdotjs } from "react-icons/si";
 import { FaNode } from "react-icons/fa";
-
+import { FaBars } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { FaXmark } from "react-icons/fa6";
+import { GiSharpCrown } from "react-icons/gi";
+import { FaHome } from "react-icons/fa";
+import { IoIosContact } from "react-icons/io";
+import { GoFileDirectoryFill } from "react-icons/go";
+import { GrServices } from "react-icons/gr";
+import { MdContactPhone } from "react-icons/md";
+
 function Navbar() {
+   const [menu, setMenu] = useState(false);
   return (
     <header>
 
@@ -21,18 +31,43 @@ function Navbar() {
     <div className="star" key={i}></div>
   ))}
 </div>
+    <nav className="w-full top-0 left-0 px-[25px] py-[15px] flex justify-between items-center z-[9999] relative">
 
+      <p className="flex items-center gap-2">
+        <img className="imgPortifolio w-[35px] rounded-full" src="./img/me.jpg" alt="" />
+        Portfolio
+      </p>
 
-        <nav>
-            <p><img className='imgPortifolio' src="./img/me.jpg" alt="" /> Portfolio</p>
-            <div className='divHome'>
-                <a href="#about">AboutMe</a>
-                <a href="#skills">Skills</a>
-                <a href="#projects">Projects</a>
-                <a href="#contact">Contact</a>
-            </div>
-          <Link to={'crut'}> <button className='but3 text-white'>Hire Me</button></Link>
-        </nav>
+      <div className="divHome hidden md:flex gap-[30px]">
+        <div className='Homediv flex items-center gap-[5px]'><IoIosContact className='w-[22px] h-[22px]'/><a href="#about">About</a></div>
+        <div className='Homediv flex items-center gap-[5px]'><GoFileDirectoryFill className='w-[22px] h-[22px]'/><a href="#about">Projects</a></div>
+        <div className='Homediv flex items-center gap-[5px]'><GrServices className='w-[22px] h-[22px]'/><a href="#about">Services</a></div>
+        <div className='Homediv flex items-center gap-[5px]'><MdContactPhone className='w-[22px] h-[22px]'/><a href="#about">Contact</a></div>
+      </div>
+
+      <Link to={"crut"} className="hidden md:block">
+        <button className="but3 text-white">Hire Me</button>
+      </Link>
+
+   
+      <div className="md:hidden text-2xl cursor-pointer">
+        {menu ? (
+          <div className='flex gap-[15px]'><GiSharpCrown className='xikin' /><FaXmark className='xikin' onClick={() => setMenu(false)} /></div>
+        ) : (
+          <div className='flex gap-[15px]'><GiSharpCrown className='xikin'/><FaBars className='xikin' onClick={() => setMenu(true)} /></div>
+        )}
+      </div>
+
+      
+      {menu && (
+        <div className="absolute top-[70px] text-white flex flex-col gap-[12px] py-8 px-6 md:hidden shadow-xl rounded-l-xl">
+        <div className='Homediv flex items-center gap-[5px]'><IoIosContact className='w-[22px] h-[22px]'/><a href="#about">About</a></div>
+        <div className='Homediv flex items-center gap-[5px]'><GoFileDirectoryFill className='w-[22px] h-[22px]'/><a href="#about">Projects</a></div>
+        <div className='Homediv flex items-center gap-[5px]'><GrServices className='w-[22px] h-[22px]'/><a href="#about">Services</a></div>
+        <div className='Homediv flex items-center gap-[5px]'><MdContactPhone className='w-[22px] h-[22px]'/><a href="#about">Contact</a></div>
+</div>
+      )}
+    </nav>
 
 
         {/* intraduction================ */}
@@ -44,7 +79,7 @@ function Navbar() {
               <div className='divMatnInto'>
                 <p>Hi, welcome to my portfolio</p>
                 <h1>I'm <span>Sherdorbek</span> Abduqahhorov</h1>
-                <p>Frontend Developer | Passionate about creating modern, responsive websites | IELTS7</p>
+                <h2>Frontend Developer</h2>
                 <div className='buttonlar'>
                     <button className='but1'><a className='abutt' href="#projects">View Projects</a></button>
                     <button className='but2'><a className='abutt' href="#contact">Contact Me</a></button>
